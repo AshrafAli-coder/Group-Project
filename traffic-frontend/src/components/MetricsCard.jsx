d66b309cbe3e85691c721231f9c34dfe9e758e16
Naveena Runku <kdwani1114@gmail.com>
2026-03-09 21:52:58 +0530

    Update MetricsCard.jsx

--- traffic-frontend/src/components/MetricsCard.jsx
+++ traffic-frontend/src/components/MetricsCard.jsx
@@ -1,16 +1,24 @@
 function MetricsCard({ title, value }) {
+
+  const formattedValue =
+    typeof value === "number"
+      ? value.toFixed(2)
+      : value || 0;
+
   return (
     <div
       style={{
-        background: "#1e1e1e",
-        padding: "20px",
-        borderRadius: "10px",
-        width: "200px",
+        background: "linear-gradient(145deg, #1e293b, #0f172a)",
+        padding: "25px",
+        borderRadius: "15px",
         textAlign: "center",
+        boxShadow: "0 0 15px rgba(0,255,136,0.2)",
       }}
     >
-      <h3>{title}</h3>
-      <h2>{value}</h2>
+      <h3 style={{ color: "#94a3b8" }}>{title}</h3>
+      <h2 style={{ fontSize: "28px", color: "#00ff88" }}>
+        {formattedValue}
+      </h2>
     </div>
   );
 }
